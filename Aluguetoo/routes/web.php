aa520eaf3680d87a5a3a3e60ae34f371a69e15c4
José Carlos <carlos.assis.jr@hotmail.com>
2026-04-03 00:47:24 -0300

    Removendo todos os arquivos do repositório

--- Aluguetoo/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoriaController;
-use App\Http\Controllers\ProdutoController;
-
-
-Route::resource('produtos', ProdutoController::class);
-Route::resource('categorias', CategoriaController::class);
